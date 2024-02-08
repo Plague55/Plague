795eb1427ad0865f6026f6db041a15c656c93e6c
Plague55 <144946046+Plague55@users.noreply.github.com>
2024-02-08 16:25:37 -0300

    Update PLaguehubV2.lua

--- PLaguehubV2.lua
+++ PLaguehubV2.lua
@@ -33,7 +33,8 @@ function usecodes()
     game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("Sub2HotSauceHan")
     game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("ToadBoi120k")
     game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("Shutdown")
-    game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("Fixes")	
+    game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("Fixes")
+    game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("Mejonson10000")
 end
 
 
