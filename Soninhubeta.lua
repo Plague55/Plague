b603a7ddf7e8c88d418faee154f4aeebe2648341
Plague55 <144946046+Plague55@users.noreply.github.com>
2024-02-11 11:25:11 -0300

    Update and rename PLaguehubeta.lua to Soninhubeta.lua

---
+++ Soninhubeta.lua
@@ -0,0 +1,94 @@
+    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+    local Window = OrionLib:MakeWindow({Name = "Sonin Hub", HidePremium = false, SaveConfig = true,IntroTexT = "Sonin Hub", ConfigFolder = "Plagueconfig"})
+    
+    --Values
+    local codes = {
+        "BlamTopSecretCodeWontWorkIfNotSubbed",
+        "GoalReached",
+        "TyFor1mVisitsPart2",
+        "TyFor1mVisitsPart1",
+        "D1SGUISED",
+        "YammoRework",
+        "TyFor10kFavREAL",
+        "NeelsTV",
+        "FinalDelay",
+        "RELEASE",
+        "SorryForDelay",
+        "Sub2KingLuffy",
+        "Sub2CodeNex77k",
+        "Sub2Blamspot524k",
+        "Sub2HotSauceHan",
+        "ToadBoi120k",
+        "Shutdown",
+        "Fixes",
+        "Mejonson10000",
+        "BlamsSecret1MilionUniquePlayerCode",
+        "Update1TrailerHYPE",
+        "BlamsAndShocksNightmare100kMemberReRollCodeTrySubscribingToBlamSpotOnYTAndFollowingFr_ShockOnTwitterIfItDoesntWork"
+    }
+
+    -- Functions
+    function summon1()
+        game:GetService("ReplicatedStorage").Remotes.Summon:InvokeServer("10","1")
+    end
+
+    function summon2()
+        game:GetService("ReplicatedStorage").Remotes.Summon:InvokeServer("10","2")
+    end
+
+    function usecodes()
+        for i, code in ipairs(codes) do
+            game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer(code)
+        end
+    end
+
+    function addCode(newCode)
+        table.insert(codes, newCode)
+    end
+
+    --Tabs
+    local MainTab = Window:MakeTab({
+        Name = "Principais Funcao",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
+
+    --Toggles
+
+    --Buttons
+    MainTab:AddButton({
+        Name = "Sumonar 10 (Primeiro Banner)",
+        Callback = function()
+                summon1()
+          end    
+    })
+
+
+    MainTab:AddButton({
+        Name = "Sumonar 10 (Segundo Banner)",
+        Callback = function()
+                summon2()
+          end    
+    })
+
+    MainTab:AddButton({
+        Name = "Use Todos Os Codigos",
+        Callback = function()
+                usecodes()
+          end    
+    })
+
+    MainTab:AddButton({
+        Name = "Adicionar Codigo",
+        Callback = function()
+            local newCode = Window:MakeTextbox({Name = "New Code", Default = "", PlaceholderText = "Enter new code"})
+            newCode:MakePopup()
+            newCode.FocusLost:Connect(function()
+                addCode(newCode.Text)
+            end)
+        end    
+    })
+
+end
+
+OrionLib:Init()
