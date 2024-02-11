b80c3acda548e657e163d79f8bc7f9ce47e124a1
Plague55 <144946046+Plague55@users.noreply.github.com>
2024-02-11 11:48:55 -0300

    Update SoninhubV2.lua

--- SoninhubV2.lua
+++ SoninhubV2.lua
@@ -1,80 +1,77 @@
--- PLague Hub
+-- Sonin
 if game.PlaceId == 12886143095 then
 
-    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-    local Window = OrionLib:MakeWindow({Name = "Sonin Hub", HidePremium = false, SaveConfig = true,IntroTexT = "Sonin Hub", ConfigFolder = "Plagueconfig"})
-    
-    --Values
-    
-    --Functions
-    function summon1()
-        game:GetService("ReplicatedStorage").Remotes.Summon:InvokeServer("10","1")
-    end
-    
-    function summon2()
-        game:GetService("ReplicatedStorage").Remotes.Summon:InvokeServer("10","2")
-    end
-    
-    function usecodes()
-        game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("BlamTopSecretCodeWontWorkIfNotSubbed")
-        game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("GoalReached")
-        game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("TyFor1mVisitsPart2")
-        game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("TyFor1mVisitsPart1")
-        game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("D1SGUISED")
-        game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("YammoRework")
-        game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("TyFor10kFavREAL")
-        game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("NeelsTV")
-        game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("FinalDelay")
-        game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("RELEASE")
-        game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("SorryForDelay")
-        game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("Sub2KingLuffy")
-        game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("Sub2CodeNex77k")
-        game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("Sub2Blamspot524k")
-        game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("Sub2HotSauceHan")
-        game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("ToadBoi120k")
-        game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("Shutdown")
-        game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("Fixes")
-        game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("Mejonson10000")
-        game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("BlamsSecret1MilionUniquePlayerCode")
-        game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("Update1TrailerHYPE")
-        game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("BlamsAndShocksNightmare100kMemberReRollCodeTrySubscribingToBlamSpotOnYTAndFollowingFr_ShockOnTwitterIfItDoesntWork")
-            
-    end
-    
-    
-    --Tabs
-    local MainTab = Window:MakeTab({
-        Name = "Principais Funcao",
-        Icon = "rbxassetid://4483345998",
-        PremiumOnly = false
-    })
-    
-    --Toggles
-    
-    --Buttons
-    MainTab:AddButton({
-        Name = "Sumonar 10 (Primeiro Banner)",
-        Callback = function()
-                summon1()
-          end    
-    })
-    
-    
-    MainTab:AddButton({
-        Name = "Sumonar 10 (Segundo Banner)",
-        Callback = function()
-                summon2()
-          end    
-    })
-    
-    MainTab:AddButton({
-        Name = "Use Todos Os Codigos",
-        Callback = function()
-                usecodes()
-          end    
-    })
-    
-    
-    end
-    
-    OrionLib:Init()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Sonin Hub", HidePremium = false, SaveConfig = true,IntroTexT = "Sonin Hub", ConfigFolder = "Soninconfig"})
+
+--Values
+
+--Functions
+function summon1()
+    game:GetService("ReplicatedStorage").Remotes.Summon:InvokeServer("10","1")
+end
+
+function summon2()
+    game:GetService("ReplicatedStorage").Remotes.Summon:InvokeServer("10","2")
+end
+
+function usecodes()
+    game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("BlamTopSecretCodeWontWorkIfNotSubbed")
+    game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("GoalReached")
+    game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("TyFor1mVisitsPart2")
+    game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("TyFor1mVisitsPart1")
+    game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("D1SGUISED")
+    game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("YammoRework")
+    game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("TyFor10kFavREAL")
+    game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("NeelsTV")
+    game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("FinalDelay")
+    game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("RELEASE")
+    game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("SorryForDelay")
+    game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("Sub2KingLuffy")
+    game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("Sub2CodeNex77k")
+    game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("Sub2Blamspot524k")
+    game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("Sub2HotSauceHan")
+    game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("ToadBoi120k")
+    game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("Shutdown")
+    game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("BlamTopSecretCodeWontWorkIfNotSubbed")
+    game:GetService("ReplicatedStorage").Remotes.ClaimCode:InvokeServer("Update1TrailerHYPE")
+end
+
+
+--Tabs
+local MainTab = Window:MakeTab({
+	Name = "Principais Funcao",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+--Toggles
+
+--Buttons
+MainTab:AddButton({
+	Name = "Sumonar 10 (Primeiro Banner)",
+	Callback = function()
+            summon1()
+  	end    
+})
+
+
+MainTab:AddButton({
+	Name = "Sumonar 10 (Segundo Banner)",
+	Callback = function()
+            summon2()
+  	end    
+})
+
+MainTab:AddButton({
+	Name = "Use Todos Os Codigos",
+	Callback = function()
+            usecodes()
+  	end    
+})
+
+
+end
+
+OrionLib:Init()
+
